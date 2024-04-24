19fe85312bbaa00bf2aff7da501f7a56b109987c
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-04-25 01:45:45 +0600

    created the blog routes without caching data

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,11 +2,13 @@ import BlogCard from "@/components/ui/BlogCard";
 import { Blog } from "../types";
 
 const BlogsPage = async () => {
-    const res = await fetch("http://localhost:5000/blogs")
+    const res = await fetch("http://localhost:5000/blogs",{
+        cache:"no-store"
+    })
     const blogs=await res.json()
 
   return (
-    <div>
+    <div className="w-[90%] mx-auto ">
       <h1 className="text-4xl text-center my-5">
        All Blogs from <span className="text-accent">Blogiz</span>
       </h1>
@@ -16,7 +18,7 @@ const BlogsPage = async () => {
           unprecedented computational power.
         </i>
           </p>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-3 gap-4 my-5">
               {
                   blogs.map((blog:Blog)=>(<BlogCard  key={blog.id} blog={blog}></BlogCard>))
               }
