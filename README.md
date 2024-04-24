f972ec20f371a7cf64edeac3050d012649db3ca0
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-04-25 02:23:57 +0600

    added the readme file for declaration redux setup

--- README.md
+++ README.md
@@ -5,3 +5,41 @@
 1. Clone the repository.
 2. Install dependencies using `npm install`.
 3. Run the project using `npm run dev`.
+
+## How to configure Redux to your project
+
+1.Install Redux
+2.create redux folder and store file
+3.create lib folder and a provider 4. Rap the layout html in That provider
+
+```
+ return (
+    <Providers>
+      <html lang="en" data-theme="light">
+      <body className={roboto.className}>
+        <Header />
+        <div className="min-h-screen">{children}</div>
+        <Footer />
+      </body>
+    </html>
+    </Providers>
+  );
+```
+
+5.create api folder and create your necessary api and config that into the store
+
+```
+  reducer: {
+        [baseApi.reducerPath]: baseApi.reducer,
+
+    },
+```
+
+6. Add middleware in the store
+
+```
+
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+```
+
--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,18 +1,24 @@
-import { BlogId } from "@/app/types";
+import { Blog, BlogId } from "@/app/types";
 import BlogDetails from "@/components/ui/BlogDetails";
 
+export const generateStaticParams = async () => {
+  const res = await fetch(`http://localhost:5000/blogs`, {
+    cache: "no-store",
+  });
+  const blogs = await res.json();
+  return blogs.slice(0, 2).map((blog: Blog) => ({ blogId: blog.id }));
+};
 
-
-const BlogDetailPage = async ({params}:BlogId) => {
-    const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
-        cache:"no-store"
-    })
-    const blog= await res.json()
-    return (
-        <div className="my-10">
-            <BlogDetails blog={blog}/>
-        </div>
-    );
+const BlogDetailPage = async ({ params }: BlogId) => {
+  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
+    cache: "no-store",
+  });
+  const blog = await res.json();
+  return (
+    <div className="my-10">
+      <BlogDetails blog={blog} />
+    </div>
+  );
 };
 
 export default BlogDetailPage;
