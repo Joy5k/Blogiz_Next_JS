8c2ba4294ddb26d1f3ff0b8bb3700e457fdb796e
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-04-25 02:39:02 +0600

    created the loading page for blogs routes

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,12 +1,16 @@
+"use client`"
+
 import BlogCard from "@/components/ui/BlogCard";
 import { Blog } from "../types";
+import { useGetBlogsQuery } from "@/redux/api/baseApi";
 
 const BlogsPage = async () => {
-    const res = await fetch("http://localhost:5000/blogs",{
-        cache:"no-store"
-    })
-    const blogs=await res.json()
-
+    // const res = await fetch("http://localhost:5000/blogs",{
+    //     cache:"no-store"
+    // })
+    // const blogs=await res.json()
+  const { data:blogs, isLoading, isError, error } = useGetBlogsQuery("")
+  
   return (
     <div className="w-[90%] mx-auto ">
       <h1 className="text-4xl text-center my-5">
